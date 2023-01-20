b6b26f663e9d7a7704252fee990d1d845d248983
codekarsatvik <skaggarwal2001.com@gmail.com>
2023-01-20 14:11:15 +0530

    port changed

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(function(err,req,res,next){
 });
 
 // listen for requests
-app.listen(process.env.port || 4000, function(){
+app.listen(process.env.port || 8080, function(){
     console.log('Ready to Go!');
 });
